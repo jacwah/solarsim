a10fe7bba41f2e24a8a084a189c1cf02d2edabb7
Jacob Wahlgren <jacob.wahlgren@gmail.com>
2017-04-22 23:03:37 +0200

    Add all planets!

--- src/main.c
+++ src/main.c
@@ -19,7 +19,7 @@
 #define PX(x) (g_render_scale * x + SCREEN_SIZE / 2)
 #define PY(y) (SCREEN_SIZE - (g_render_scale * y + SCREEN_SIZE / 2))
 
-const double g_render_scale = SCREEN_SIZE / 4.0e11;
+const double g_render_scale = SCREEN_SIZE / 1.0e13;
 
 TTF_Font *g_font = NULL;
 
@@ -49,10 +49,45 @@ Body g_solar_system[] = {
 		.mass = 1.9885e30,
 		.name = "Sol"
 	}, {
+		.position = { .x = -5.2726e10, .y = -3.7468e10 },
+		.velocity = { .x = 1.8415e4, .y = -3.7402e4 },
+		.mass = 3.302e23,
+		.name = "Mercurius"
+	}, {
+		.position = { .x = -7.0490e10, .y = -8.1077e10 },
+		.velocity = { .x = 2.6200e4, .y = -2.3102e4 },
+		.mass = 48.685e23,
+		.name = "Venus"
+	}, {
 		.position = { .x = -1.2732e11, .y = -7.9589e10 },
 		.velocity = { .x = 1.5212e4, .y = -2.5423e4 },
 		.mass = 5.9722e24,
 		.name = "Tellus"
+	}, {
+		.position = { .x = 4.7562e10, .y = 2.2625e11 },
+		.velocity = { .x = -2.2806e4, .y = 7.0263e3 },
+		.mass = 6.4185e23,
+		.name = "Mars"
+	}, {
+		.position = { .x = -7.7066e11, .y = -2.6598e11 },
+		.velocity = { .x = 4.1098e3, -1.1732e4 },
+		.mass = 1898.13e24,
+		.name = "Jupiter"
+	}, {
+		.position = { .x = -1.9262e11, .y = -1.4907e12 },
+		.velocity = { .x = 9.0486e3, .y = -1.2687e3 },
+		.mass = 5.6832e26,
+		.name = "Saturnus"
+	}, {
+		.position = { .x = 2.7173e12, .y = 1.2281e12 },
+		.velocity = { .x = -2.8545e3, .y = 5.8881e3 },
+		.mass = 86.8103e24,
+		.name = "Uranus"
+	}, {
+		.position = { .x = 4.2558e12, .y = -1.3998e12 },
+		.velocity = { .x = 1.6627e3, .y = 5.1961e3 },
+		.mass = 102.41e24,
+		.name = "Neptunus"
 	}
 };
 
@@ -216,8 +251,8 @@ int MainLoop(SDL_Renderer *renderer)
 		SDL_FreeSurface(surf);
 	}
 
-	// 1 week per second
-	double delta_time = 1.0 / 60.0 * 60.0 * 60.0 * 24.0 * 7.0;
+	// 10 weeks per second
+	double delta_time = 1.0 / 60.0 * 60.0 * 60.0 * 24.0 * 7.0 * 10.0;
 
 	while (!exiting) {
 		while (SDL_PollEvent(&event)) {
